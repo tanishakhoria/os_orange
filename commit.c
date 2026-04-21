d598995d9d2d9be017da0301013c35d19b6abc66
Tanisha <khoriatanisha@gmail.com>
2026-04-21 17:47:47 +0530

    Update commit.c

--- commit.c
+++ commit.c
@@ -197,23 +197,7 @@ extern int tree_from_index(ObjectID *id_out);
 //   - head_update       : moves the branch pointer to your new commit
 //
 // Returns 0 on success, -1 on error.
-int commit_create(const char *message, ObjectID *commit_id_out) {
-    Commit commit;
-    
-    // Step 1: Build tree from index
-    if (tree_from_index(&commit.tree) != 0) {
-        return -1;
-    }
-    
-    // Step 2: Get parent commit (if exists)
-    ObjectID parent_id;
-    if (head_read(&parent_id) == 0) {
-        commit.has_parent = 1;
-        commit.parent = parent_id;
-    } else {
-        commit.has_parent = 0;
-    }
-    
+
     // Step 3: Set author and timestamp
 
     
