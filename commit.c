549d678154db608eead593ba6d7b7d0e3d159f7b
Tanisha <khoriatanisha@gmail.com>
2026-04-21 17:45:26 +0530

    Update commit.c

--- commit.c
+++ commit.c
@@ -238,10 +238,3 @@ int commit_create(const char *message, ObjectID *commit_id_out) {
     }
     free(commit_data);
     
-    // Step 7: Update HEAD
-    if (head_update(commit_id_out) != 0) {
-        return -1;
-    }
-    
-    return 0;
-}
